d1efe1cf6d07c7ee626b14f355b582e14d00ee14
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2018-11-24 14:10:58 -0500

    added margin bottom to citation list, reorganized css properties

--- src/containers/CitationList.js
+++ src/containers/CitationList.js
@@ -6,15 +6,15 @@ import CitationListFilters from '../components/CitationListFilters';
 
 const CitationList = (props) => (
   <div className="content-container__citation-list">
-    <h1 className="citation-list__title">Case List</h1>
+    <h1 className="citation-list__title">Citation List</h1>
     <div className="citation-list-filter-container">
       <CitationListFilters />
     </div>
     <div className="list-header">
-      <div className="list-header__citation">Citation</div>
-      <div className="list-header__date">Date</div>
-      <div className="list-header__type">Type</div>
-      <div className="list-header__note">Note</div>
+      <div className="citation">Citation</div>
+      <div className="citation__date">Date</div>
+      <div className="citation__type">Type</div>
+      <div className="citation__note">Note</div>
       <button className="list-header__button"><i className="fa fa-lg fa-trash" aria-hidden="true"></i></button>
     </div>
     <div>
