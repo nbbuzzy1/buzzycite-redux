bf3325f08c72ea0ff6f83bd77a433be3d81163e5
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2018-11-26 06:57:57 -0500

    added firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,37 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyAS6mukhTKc-YEio0IRcWolYRAlG-vcw_c",
+  authDomain: "buzzycite-ab3af.firebaseapp.com",
+  databaseURL: "https://buzzycite-ab3af.firebaseio.com",
+  projectId: "buzzycite-ab3af",
+  storageBucket: "buzzycite-ab3af.appspot.com",
+  messagingSenderId: "1062035939966"
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-export { firebase, database as default };
+database.ref().set({
+  name: 'Nick Buzzy',
+  age: 26,
+  isSingle: false,
+  location: {
+    city: 'Cleveland',
+    country: 'United States'
+  },
+})
+
+// database.ref().set('This is my data')
+
+database.ref('age').set(27);
+database.ref('location/city').set('Chicago');
+database.ref('attributes').set({
+  height: 73,
+  weight: 180
+});
+
+// const database = firebase.database();
+
+// export { firebase, as default };
