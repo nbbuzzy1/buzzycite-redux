1139f1f98942bb763407cf962295ce57daf0516e
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2018-12-01 20:41:30 -0500

    added prompt to googleAuthProvider

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,6 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -13,5 +15,8 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+googleAuthProvider.setCustomParameters({
+  'prompt': 'select_account'
+});
 
 export { firebase, googleAuthProvider, database as default };
